658e5429a682189364a64801e31049cc01a52ded
CharlieDev85 <kikemarroquin@gmail.com>
2016-10-02 15:34:43 -0600

    un file por cada carpeta

---
+++ Logica/logica del negocio.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carle
+ * Date: 2/10/2016
+ * Time: 3:33 PM
+ */
---
+++ UI/Modelos/modelos.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carle
+ * Date: 2/10/2016
+ * Time: 3:32 PM
+ */
+
+echo "modelos.php";
