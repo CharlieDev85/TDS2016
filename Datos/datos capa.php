68e3a0d2a83a8c1fdea265d8e534d68b02360619
CharlieDev85 <kikemarroquin@gmail.com>
2016-10-02 16:34:15 -0600

    cambio comentarios en datos y logica

--- Datos/datos capa.php
+++ Datos/datos capa.php
@@ -4,4 +4,5 @@
  * User: carle
  * Date: 2/10/2016
  * Time: 3:33 PM
+ * Codigo para la capa de datos
  */
--- Logica/logica del negocio.php
+++ Logica/logica del negocio.php
@@ -4,4 +4,5 @@
  * User: carle
  * Date: 2/10/2016
  * Time: 3:33 PM
+ * codigo para la capa de logica del negocio
  */
